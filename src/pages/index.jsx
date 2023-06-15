f894cb2f3893a322ddafebb45d84650e36f76b76
Yakov Rakhamimov <yakovr@wix.com>
2023-06-15 12:10:07 +0300

    homepage header and styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,8 +4,16 @@ import CodeBlock from '@theme/CodeBlock';
 import MyComponentSource from '!!raw-loader!./snippet';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-
 import styles from './index.module.css';
+
+function Header() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>{siteConfig.title}</h1>
+    </header>
+  );
+}
 
 export default function Home() {
   const {
@@ -15,6 +23,7 @@ export default function Home() {
   return (
     <Layout description={tagline}>
       <div className={styles.container}>
+      <Header />
         <div className='col col--5'>
           <MDXContent>
             <CodeBlock language='ts'>{MyComponentSource}</CodeBlock>
